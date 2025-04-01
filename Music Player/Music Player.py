3ef7ff616d85c0d87ad522e0839ee21a1e024074
Fash Mayor <mayowasamson03@gmail.com>
2025-04-01 18:26:50 +0100

    Update music player
    Added a "songLabel" widget to display the title of song being played.
    Added an "albumImage" widget to display an image

--- Music Player/Music Player.py
+++ Music Player/Music Player.py
@@ -7,7 +7,10 @@ from kivymd.app import MDApp
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.button import Button
 from kivymd.uix.button import MDIconButton
+from kivy.uix.label import Label
+from kivy.uix.image import Image
 
+from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
 from kivy.core.window import Window
 from kivy.graphics import Rectangle
@@ -20,7 +23,7 @@ class MusicPlayerApp(MDApp):
         layout = RelativeLayout()
 
         with layout.canvas.before:
-            Color(0.2, 0.9, 0.6)
+            Color(1.2, 1.9, 0.7, 1)
             self.rect = Rectangle(size = layout.size, pos = layout.pos)
             layout.bind(size = self._update_rect, pos = self._update_rect)
 
@@ -29,27 +32,38 @@ class MusicPlayerApp(MDApp):
         #print(self.music_files)
 
         self.song_list = [x for x in self.music_files if x.endswith(".mp3")]
-        # print(self.song_list)
+        print(self.song_list)
 
         self.song_count = len(self.song_list)
         print(self.song_count)
 
+        self.songLabel = Label(pos_hint = {"center_x": 0.5, "center_y": 0.96},
+                               size_hint = (1, 1), font_size = 20, color = (0, 0, 0, 1))
+        
+        self.albumImage = Image(pos_hint = {"center_x": 0.5, "center_y": 0.55},
+                                size_hint = (0.8, 0.75))
+
         self.playButton = MDIconButton(pos_hint = {"center_x": 0.4, "center_y": 0.05}, 
                                      icon = "play-circle-regular-24.png", on_press = self.playAudio)
         
-        self.pauseButton = MDIconButton(pos_hint = {"center_x": 0.55, "center_y": 0.05}, 
-                                     icon = "pause-circle-regular-24.png", on_press = self.stopAudio)
+        self.stopButton = MDIconButton(pos_hint = {"center_x": 0.55, "center_y": 0.05}, 
+                                     icon = "pause-circle-regular-24.png", on_press = self.stopAudio, disabled = True)
 
-
+        layout.add_widget(self.songLabel)
+        layout.add_widget(self.albumImage)
         layout.add_widget(self.playButton)
-        layout.add_widget(self.pauseButton)
+        layout.add_widget(self.stopButton)
+        Clock.schedule_once(self.playAudio)
         return layout
     
     def playAudio(self, obj):
         self.song_title = self.song_list[random.randrange(0, self.song_count)]
         print(self.song_title)
         self.sound = SoundLoader.load("{}/{}".format(self.music_dir, self.song_title))
+        self.songLabel.text = self.song_title[0:-4]
+        self.albumImage.source = "C:/Users/Fash Mayor/OneDrive/Documents/Getting Started/Lnt/Py/Python Project Apps/The-Shed-Project/Music Player/music_dir/gsp Cover.png"
         self.sound.play()
+        self.stopButton.disabled = False
 
     def stopAudio(self, obj):
         self.sound.stop()
