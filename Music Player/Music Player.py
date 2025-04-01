75479ee02f5f44c53e6e2c7d7aae94280b397305
Fash Mayor <mayowasamson03@gmail.com>
2025-04-01 17:58:42 +0100

    Enabling the play and stop button
    Added a music directory with songs and pictures.
    Enabled the play button to be able to start playing a random song.
    Enabled the pause button to be able to stop current song playing.

--- Music Player/Music Player.py
+++ Music Player/Music Player.py
@@ -24,15 +24,35 @@ class MusicPlayerApp(MDApp):
             self.rect = Rectangle(size = layout.size, pos = layout.pos)
             layout.bind(size = self._update_rect, pos = self._update_rect)
 
+        self.music_dir = "/Users/Fash Mayor/OneDrive/Documents/Getting Started/Lnt/Py/Python Project Apps/The-Shed-Project/Music Player/music_dir"
+        self.music_files = os.listdir(self.music_dir)
+        #print(self.music_files)
+
+        self.song_list = [x for x in self.music_files if x.endswith(".mp3")]
+        # print(self.song_list)
+
+        self.song_count = len(self.song_list)
+        print(self.song_count)
+
         self.playButton = MDIconButton(pos_hint = {"center_x": 0.4, "center_y": 0.05}, 
-                                     icon = "play-circle-regular-24.png")
+                                     icon = "play-circle-regular-24.png", on_press = self.playAudio)
+        
         self.pauseButton = MDIconButton(pos_hint = {"center_x": 0.55, "center_y": 0.05}, 
-                                     icon = "pause-circle-regular-24.png")
+                                     icon = "pause-circle-regular-24.png", on_press = self.stopAudio)
 
 
         layout.add_widget(self.playButton)
         layout.add_widget(self.pauseButton)
         return layout
+    
+    def playAudio(self, obj):
+        self.song_title = self.song_list[random.randrange(0, self.song_count)]
+        print(self.song_title)
+        self.sound = SoundLoader.load("{}/{}".format(self.music_dir, self.song_title))
+        self.sound.play()
+
+    def stopAudio(self, obj):
+        self.sound.stop()
 
     def _update_rect(self, instance, value):
         self.rect.pos = instance.pos
